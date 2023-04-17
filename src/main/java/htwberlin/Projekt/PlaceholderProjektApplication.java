31b06d9c1a8f58a16e125bbec636784bcf9b4213
Moslem-Elo <moslemeo@icloud.com>
2023-04-17 16:12:22 +0200

    Test

---
+++ src/main/java/htwberlin/Projekt/PlaceholderProjektApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.Projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceholderProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceholderProjektApplication.class, args);
+	}
+
+}
