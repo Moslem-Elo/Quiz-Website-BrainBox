3add8fe5d55d99c712ad67b18863eb26d5b7b583
Moslem El-Omar <107929914+Moslem-Elo@users.noreply.github.com>
2023-05-14 16:47:21 +0200

    Update Answer.java

--- src/main/java/htwberlin/Projekt/Entitys/Answer.java
+++ src/main/java/htwberlin/Projekt/Entitys/Answer.java
@@ -8,38 +8,17 @@ import org.springframework.data.annotation.Id;
 
 import java.util.Objects;
 
-@Entity
 public class Answer {
-
-
-    @jakarta.persistence.Id
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
 
     private String answerText;
 
 
     private boolean correct;
 
-    @ManyToOne()
-    private Question question;
-
-    public Answer() {}
-
-    public Answer(String answerText, boolean correct, Question question) {
+    public Answer(String answerText, boolean correct) {
         this.answerText = answerText;
         this.correct = correct;
-        this.question = question;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getAnswerText() {
@@ -58,23 +37,4 @@ public class Answer {
         this.correct = correct;
     }
 
-    public Question getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(Question question) {
-        this.question = question;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Answer answer)) return false;
-        return correct == answer.correct && id.equals(answer.id) && answerText.equals(answer.answerText) && question.equals(answer.question);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, answerText, correct, question);
-    }
 }
