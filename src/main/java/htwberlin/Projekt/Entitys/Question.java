4b0f223b210bbfcc31050b2c3847381d704c09d8
Moslem El-Omar <107929914+Moslem-Elo@users.noreply.github.com>
2023-05-14 16:47:49 +0200

    Update Question.java

--- src/main/java/htwberlin/Projekt/Entitys/Question.java
+++ src/main/java/htwberlin/Projekt/Entitys/Question.java
@@ -7,44 +7,20 @@ import java.util.List;
 
 
 
-@Entity
 public class Question {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    @Column(nullable = false)
+
     private String text;
 
-    @ManyToOne
-    @JoinColumn(name = "quiz_id")
-    private Quiz quiz;
-
-    @OneToMany(mappedBy = "answer")
     private List<Answer> answers = new ArrayList<>();
-
-    //Todo antworten in die question klasse implentieren answer als kLASSE UND dann in einen answer
-    // ist packen mit dem Datentyp answer der den Boolean coreect beinhaltet
-
-    //Todo Theme als enum deklarieren
-    //ToDo question als Liste in Quiz implentieren der DatenTyp Question
 
     public Question() {
     }
 
-    public Question(String text, Quiz quiz) {
+    public Question(String text) {
         this.text = text;
-        this.quiz = quiz;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getText() {
         return text;
@@ -52,14 +28,6 @@ public class Question {
 
     public void setText(String text) {
         this.text = text;
-    }
-
-    public Quiz getQuiz() {
-        return quiz;
-    }
-
-    public void setQuiz(Quiz quiz) {
-        this.quiz = quiz;
     }
 
     public List<Answer> getAnswers() {
